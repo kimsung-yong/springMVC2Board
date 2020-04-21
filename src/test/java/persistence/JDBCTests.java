ee499eacbcacfec7c8a43b79ac7d447f5c6316ff
kimsungyoeng <anrl1014@naver.com>
2020-04-22 02:48:15 +0900

    4.22

---
+++ src/test/java/persistence/JDBCTests.java
@@ -0,0 +1,28 @@
+package persistence;
+
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@Log4j
+public class JDBCTests {
+
+    static{
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void testConnection(){
+
+        try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","mvc","mvc")){
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
